99181c3752d1e4ac510e0492ce5b0b5cbfbd1863
Amirreza Kheradmand <amir.rezakheradmand.2017@gmail.com>
2021-02-19 20:16:51 +0330

    first commit

---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    
+    <title>ای-کمپلکس</title>
+</head>
+<body>
+  <!-- BEGIN: Navigation -->
+    <nav class="navbar navbar-dark navbar-expand-lg fixed-top">
+      <a href="#" class="navbar-brand">
+        <img src="img/logo.png" alt="" width="60px">
+      </a>
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav ml-auto">
+            <li clas="navbar-item">
+              <a href="http://arvin.acomplex.ir" class="nav-link active mr-5">آروین</a>
+            </li>  
+            <li clas="navbar-item">
+              <a href="http://amaj.acomplex.ir" class="nav-link active mr-5">آماج</a>
+            </li>  
+            <li clas="navbar-item">
+              <a href="http://shop.acomplex.ir" class="nav-link active mr-5">فروشگاه</a>
+            </li> 
+            <li clas="navbar-item">
+              <a href="http://reg.acomplex.ir" class="nav-link active mr-5">سامانه درخواست</a>
+            </li> 
+        </ul>
+      </div>
+    </nav>
+<!-- END: Navigation -->
+  <div class="container-fluid">
+    <div class="row">
+        <div class="col-xs-12 col-md-6">
+            <div class="bg bg-left">
+              <div class="amaj text-center">
+              <img src="img/amaj.svg" alt="" style="width: 250px;margin-top:-100px;">
+              <br>
+              <br>
+              <h1 style="font-size: 35px !important;">گروه مشاوره آماج</h1>
+              <br>
+                <a href="http://amaj.acomplex.ir" class="btn btn-light">ورود به آماج</a>
+              </div>
+            </div>
+        </div>
+        <div class="col-xs-12 col-md-6">
+              <div class="bg bg-right">
+              <div class="arvin text-center">
+              <img src="img/arvin.svg" alt="" style="width: 250px;margin-top:-100px;">
+              <br>
+              <br>
+              <h1 style="font-size: 35px !important;">گروه معماری آروین</h1>
+              <br>
+              <a href="http://arvin.acomplex.ir" class="btn btn-warning">ورود به آروین</a>
+              </div>
+            </div>
+        </div>
+    </div>
+  </div>
+<section class="section_two text-center">
+  <div class="container-fluid">
+    <div class="row">
+          <a href="http://shop.acomplex.ir/my-account/" class="col-md-4 box-hv">
+            <h3>
+              حساب کابری
+            </h3>
+            <p>لورم</p>
+          </a>
+          <a href="http://shop.acomplex.ir/" class="col-md-4 box-hv">
+            <h3>
+              فروشگاه
+            </h3>
+            <p>لورم</p>
+          </a>
+          <a href="http://reg.acomplex.ir" class="col-md-4 box-hv">
+              <h3>
+                سامانه
+              </h3>
+              <p>لورم</p>
+          </a>
+    </div>
+  </div>
+</section>
+<footer class="footer">
+  <div class="container text-center">
+    <span class="">
+      همه‌ی حقوق برای کمپلکس آ میباشد.
+    </span>
+    <br>
+    <span>
+      Powered By 
+      <a href="http://imgx.ir">IMGX</a>
+    </span>
+  </div>
+</footer>
+    <!-- scripts  -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
